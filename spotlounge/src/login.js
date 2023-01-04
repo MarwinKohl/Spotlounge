4d4a34cea8b7444431070fba04b505d036efe973
Nicolaj <nicolajhaueter72@gmail.com>
2023-01-04 10:21:26 +0100

    Login Client ID fixed

--- spotlounge/src/login.js
+++ spotlounge/src/login.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container } from 'react-bootstrap'
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=d191edcba8b84b3a939f442b560fe205=&response_type=code&redirect_uri=https://Localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=d191edcba8b84b3a939f442b560fe205&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
     return (
@@ -11,7 +11,7 @@ export default function Login() {
         style={{ minHeight: "100vh" }}
       >
         <a className="btn btn-success btn-lg" href={AUTH_URL}>
-          Login With Spotify
+          Login with Spotify
         </a>
       </Container>
     )
