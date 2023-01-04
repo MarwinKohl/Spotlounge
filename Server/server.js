3441025ad653c4ae37d96534ec75d3f8e2cf0a7c
Nicolaj <nicolajhaueter72@gmail.com>
2023-01-04 11:13:49 +0100

    Added Dashboard for server.js

---
+++ Server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+const app = express();
+
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'https://localhost:3000',
+        clientId: 'd191edcba8b84b3a939f442b560fe205',
+        clientSecret: 'a4d93f16b5864e9d8b2b7e9117dde0df'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch((err) => {
+        res.sendStatus(400)
+    })
+})
--- spotlounge/src/App.js
+++ spotlounge/src/App.js
@@ -1,8 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './login';
+import Dashboard from './Dashboard';
+
+const code = new URLSearchParams(window.location.search).get
+('code')
 
 function App() {
-  return < Login />
+  return code ? <Dashboard code={code} /> :< Login />
+
   
 }
 
