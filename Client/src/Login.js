7f63e115f1b37d6c1a54677a821352140ef1a304
Nicolaj <nicolajhaueter72@gmail.com>
2023-01-05 11:10:54 +0100

    Fixed error and 2 new files

--- Client/src/Login.js
+++ Client/src/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Container } from "react-bootstrap"
 
-import { Container } from 'react-bootstrap'
-
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=d191edcba8b84b3a939f442b560fe205&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = 
+  "https://accounts.spotify.com/authorize?client_id=d191edcba8b84b3a939f442b560fe205&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
     return (
--- Client/src/useAuth.js
+++ Client/src/useAuth.js
@@ -5,8 +5,10 @@ export default function useAuth(code) {
     const [accessToken, setAccessToken] = useState();
     const [refreshToken, setRefreshToken] = useState();
     const [expiresIn, setExpiresIn] = useState();
+    
     useEffect(()=>{
-        axios.post('http://localhost:3001/login', {
+      axios
+        .post('http://localhost:3001/login', {
             code,
         })
         .then(res => {
@@ -14,11 +16,30 @@ export default function useAuth(code) {
             setRefreshToken(res.data.refreshToken)
             setExpiresIn(res.data.expiresIn)
             window.history.pushState({}, null, "/")
-          })
+        })
         .catch(() => {
-        window.location = "/"
+          window.location = "/"
         })
     }, [code])
+
+    useEffect(()=>{
+        if (!refreshToken || !expiresIn) return
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:3001/refresh", {
+          refreshToken,
+        })
+        .then(res => {
+          setAccessToken(res.data.accessToken)
+          setExpiresIn(res.data.expiresIn)
+        })
+        .catch(() => {
+          window.location = "/"
+        })
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearInterval(interval)
+  }, [refreshToken, expiresIn])
 
     return accessToken;
 }
