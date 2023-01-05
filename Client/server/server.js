e2eb965a3dffb9836e2ee10ff9f9e28869139be4
Nicolaj <nicolajhaueter72@gmail.com>
2023-01-05 13:14:42 +0100

    Added Ly

--- Client/server/server.js
+++ Client/server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const lyricsFinder = require("lyrics-finder")
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
